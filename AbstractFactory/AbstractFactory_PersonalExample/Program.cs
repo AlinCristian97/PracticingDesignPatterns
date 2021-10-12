0c1a71153a4457d198c237c8117af3fe4eae0c03
Alin Apostol <alin.cristian977@gmail.com>
2021-10-12 23:11:53 +0300

    Added AbstractFactory project

---
+++ AbstractFactory/AbstractFactory_PersonalExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AbstractFactory_PersonalExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TODO: Add personal example
+        }
+    }
+}
